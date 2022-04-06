5db187d01bd703daf4a104c11928797f2d01383a
yonsun <wenjiahua@kuaishou.com>
2022-04-06 10:23:27 +0800

    修改time到第一纬

--- pre-eval/pytorch_pre1.4/NpyConvertNc.py
+++ pre-eval/pytorch_pre1.4/NpyConvertNc.py
@@ -6,14 +6,14 @@ import os
 
 
 # path传入的是npy文件的路径
-def createDistanceNc(config_dict, npyPath, ncPath ='test.nc'):
-    f_w = nc.Dataset('wjhTest.nc', 'w', format='NETCDF4')  # 创建一个格式为.nc的，名字为 ‘hecheng.nc’的文件
+def createDistanceNc(config_dict, ncPath ='test.nc'):
+    f_w = nc.Dataset('Equal_distance.nc', 'w', format='NETCDF4')  # 创建一个格式为.nc的
     # 确定基础变量的维度信息。相对与坐标系的各个轴(x,y,z)
     f_w.createDimension('south_north', config_dict['GridRowColNum'])
     f_w.createDimension('west_east', config_dict['GridRowColNum'])
     f_w.createDimension('Time', config_dict['ForecastHourNum'])
     # 创建变量。参数依次为：‘变量名称’，‘数据类型’，‘基础维度信息’
-    f_w.createVariable('Flash_pre', np.float32, ('south_north', 'west_east', 'Time'))
+    f_w.createVariable('Flash_pre', np.float32, ('Time', 'south_north', 'west_east'))
 
     # 按小时来写入结果
     for hour_plus in range(config_dict['ForecastHourNum']):
@@ -26,9 +26,17 @@ def createDistanceNc(config_dict, npyPath, ncPath ='test.nc'):
 
     f_w.variables['Flash_pre'].description = 'hourly grid prediction lightning'
     f_w.variables['Flash_pre'].coordinates = 'XLONG XLAT'
-    f_w.variables['Flash_pre'].init_time = '20201003_210000'
 
-    f_w.variables['Flash_pre'].valid_time = '20201003_220000'
+    # 获取开始时间
+    start_time = datetime.datetime.strptime(config_dict['Datetime'],'%Y%m%d%H%M')
+    # 获取结束时间
+    end_time = datetime.datetime.strptime(config_dict['Datetime'],'%Y%m%d%H%M') + datetime.timedelta(hours=config_dict['ForecastHourNum'])
+
+
+
+    f_w.variables['Flash_pre'].init_time = start_time.strftime('%Y%m%d') + '_' + start_time.strftime('%H%M%S')
+
+    f_w.variables['Flash_pre'].valid_time = end_time.strftime('%Y%m%d') + '_' + end_time.strftime('%H%M%S')
 
     # 关闭文件
     f_w.close()
@@ -39,11 +47,4 @@ def createDistanceNc(config_dict, npyPath, ncPath ='test.nc'):
 if __name__ == "__main__":
 
     config_dict = read_config()
-    # 按小时来写入结果
-    for hour_plus in range(config_dict['ForecastHourNum']):
-        # 首先获取他写入的路径
-        dt_d = datetime.datetime.strptime(config_dict['Datetime'], '%Y%m%d%H%M') + datetime.timedelta(hours=hour_plus)
-        equal_dis_path = os.path.join(config_dict['ResultDistanceSavePath'], '{}_h{}.npy'.format(dt_d.strftime('%Y%m%d%H%M'), hour_plus))
-
-
     createDistanceNc(config_dict)
